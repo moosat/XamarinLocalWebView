1f5f27e5f3791dd3bb7ab97f326bd2b66064efa5
Moshe Taieb <moshe.taieb@kodak.com>
2016-01-04 21:10:37 +0200

    PhotoSwipeGallery !!!!

--- WebViewGalleryApp/WebViewGalleryApp/MainPage.xaml.cs
+++ WebViewGalleryApp/WebViewGalleryApp/MainPage.xaml.cs
@@ -14,6 +14,11 @@ namespace WebViewGalleryApp
     {
         private List<string> _coludinaryIds = new List<string>()
         {
+            "MoSamples/pic_30.jpg",
+            "MoSamples/pic_31.jpg",
+            "MoSamples/pic_32.jpg",
+            "MoSamples/pic_33.jpg",
+            "MoSamples/pic_34.jpg",
             "MoSamples/pic_1.jpg",
             "MoSamples/pic_2.jpg",
             "MoSamples/pic_3.jpg",
@@ -67,26 +72,41 @@ namespace WebViewGalleryApp
                                     <p><a href=""local.html"">next page</a></p>
                                 </body>
                                 </html>";
-            htmlSource.Html = GetHtmlSource();
+            //htmlSource.Html = GetHtmlSource();
+            htmlSource.Html = GetPhotoSwipeHtml();
             Browser.Source = htmlSource;
         }
 
 
-                //<a class="vlightbox1" href="http://res.cloudinary.com/kodakbluesky/image/upload/v1451847955/da960d4e-0284-4e6c-89c3-69ba49a9db1e/da960d4e-0284-4e6c-89c3-69ba49a9db1e/1421656088-1_-_Copy.jpg" title=""><img src = "http://res.cloudinary.com/kodakbluesky/image/upload/t_Scale120/v1451847955/da960d4e-0284-4e6c-89c3-69ba49a9db1e/da960d4e-0284-4e6c-89c3-69ba49a9db1e/1421656088-1_-_Copy.jpg" /></ a >
+        private string GetPhotoSwipeHtml()
+        {
+            const string replaceClause = "####IMAGES_LINES_GOWSHERE###";
+
+            string template = GetHtmlTemplate("SwipeHtmlTemplate.html");
+
+            string lines = BuildPhotoSwipeImagesLines(_coludinaryIds);
+
+            string sourceHtml = template.Replace(replaceClause, lines);
+
+            return sourceHtml;
+        }
+
+
+        //<a class="vlightbox1" href="http://res.cloudinary.com/kodakbluesky/image/upload/v1451847955/da960d4e-0284-4e6c-89c3-69ba49a9db1e/da960d4e-0284-4e6c-89c3-69ba49a9db1e/1421656088-1_-_Copy.jpg" title=""><img src = "http://res.cloudinary.com/kodakbluesky/image/upload/t_Scale120/v1451847955/da960d4e-0284-4e6c-89c3-69ba49a9db1e/da960d4e-0284-4e6c-89c3-69ba49a9db1e/1421656088-1_-_Copy.jpg" /></ a >
 
         private string GetHtmlSource()
         {
             const string replaceClause = "####IMAGES_LINES_GOWSHERE###";
-            string template = GetHtmlTemplate();
+            string template = GetHtmlTemplate("HtmlTemplate.html");
             string lines = BuildImagesLines(_coludinaryIds);
             string sourceHtml = template.Replace(replaceClause, lines);
             return sourceHtml;
         }
 
-        private string GetHtmlTemplate()
+        private string GetHtmlTemplate(string resourceFile)
         {
             var assembly = typeof(MainPage).GetTypeInfo().Assembly;
-            Stream stream = assembly.GetManifestResourceStream("WebViewGalleryApp.Resources.HtmlTemplate.html");
+            Stream stream = assembly.GetManifestResourceStream($"WebViewGalleryApp.Resources.{resourceFile}");
             string text = "";
             using (var reader = new System.IO.StreamReader(stream))
             {
@@ -94,6 +114,31 @@ namespace WebViewGalleryApp
             }
             return text;
         }
+
+        
+
+        private string BuildPhotoSwipeImagesLines(List<string> cloudinaryIds)
+        {
+            const string cloudinaryBaseUrl = "https://res.cloudinary.com/kodakbluesky/image/upload/";
+            const string cloudinaryScaleThumbnail = "c_fit,h_80,w_120/";
+            const string cloudinaryScaleImage = "c_fit,w_1200,h_800/";
+            StringBuilder sb = new StringBuilder();
+            foreach (var cloudinaryId in cloudinaryIds)
+            {
+                //string fullImageUrl = $"{cloudinaryBaseUrl}{cloudinaryId}";
+                string scaledImageUrl = $"{cloudinaryBaseUrl}{cloudinaryScaleImage}{cloudinaryId}";
+                string scaledThumbnailUrl = $"{cloudinaryBaseUrl}{cloudinaryScaleThumbnail}{cloudinaryId}";
+
+                string lineTemplate = $@"<figure itemprop='associatedMedia' itemscope itemtype='http://schema.org/ImageObject'>
+                                         <a href='{scaledImageUrl}' itemprop='contentUrl' data-size='1200x800'>
+                                         <img src='{scaledThumbnailUrl}' itemprop='thumbnail' alt='Image description'/>
+                                         </a>
+                                         </figure>";
+                sb.Append(lineTemplate);
+            }
+            return sb.ToString();
+        }
+
 
         private string BuildImagesLines(List<string> cloudinaryIds)
         {            
